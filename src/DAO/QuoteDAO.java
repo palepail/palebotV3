2e690eaa817031f18f1de01e079c94432812fd97
unknown <palepail@gmail.com>
2015-07-24 12:39:01 -1000

    problems with driver not found

---
+++ src/DAO/QuoteDAO.java
@@ -0,0 +1,25 @@
+package DAO;
+
+import models.Quote;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.spi.PersistenceProvider;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by palepail on 7/24/2015.
+ */
+public class QuoteDAO {
+    PersistenceProvider provider = new HibernatePersistenceProvider();
+    EntityManagerFactory factory = provider.createEntityManagerFactory("NewPersistenceUnit", new HashMap());
+    EntityManager em = factory.createEntityManager();
+
+    public  List<Quote> getAll(){
+     Query query = em.createQuery("SELECT e FROM models.Quote e");
+     return query.getResultList();
+    }
+}
--- src/Endpoints/Quotes.java
+++ src/Endpoints/Quotes.java
@@ -1,27 +1,26 @@
 package Endpoints;
 
 
+import DAO.QuoteDAO;
 import models.Quote;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * Created by palepail on 7/23/2015.
  */
 @Path("/quotes")
 public class Quotes {
+    QuoteDAO dao = new QuoteDAO();
+
     @GET
     @Produces("application/json")
-    public Quote getAll() {
+    public List<Quote> getAll() {
 
-    Quote quote = new Quote();
-        quote.setAuthor("Derek");
-        quote.setId(1);
-        return quote;
+        return dao.getAll();
 
     }
 }
