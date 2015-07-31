a165e5767df555a596da2ff47c6cc3464a613caa
unknown <palepail@gmail.com>
2015-07-31 00:07:39 -1000

    palebot sending message

--- src/bot/DefaultListener.java
+++ src/bot/DefaultListener.java
@@ -2,16 +2,25 @@ package bot;
 
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.types.GenericMessageEvent;
+import org.pircbotx.hooks.events.MessageEvent;
 
 /**
  * Created by palepail on 7/26/2015.
  */
 public class DefaultListener extends ListenerAdapter{
     @Override
-    public void onGenericMessage(GenericMessageEvent event) {
-        //When someone says ?helloworld respond with "Hello World"
-        if (event.getMessage().startsWith("!helloworld"))
+    public void onMessage(MessageEvent event) {
+        //When someone says helloworld respond with "Hello World"
+        if (event.getMessage().startsWith("!helloworld")) {
             event.respond("Hello world!");
+        }
+        if(event.getMessage().startsWith("!palebot")) {
+            event.getBot().sendIRC().message(event.getChannel().getName(), "I'm palebot");
+        }
+
+
+
     }
+
 
 }
